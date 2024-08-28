69629d8f56053c37476bf5eeb7a4da2c225cff88
Md Sabbir Khan <mdsabbirkhan1972@gmail.com>
2024-08-29 01:10:01 +0600

    profile

--- src/Pages/Profile_Update/Profile_Update.jsx
+++ src/Pages/Profile_Update/Profile_Update.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
 import './Profile_Update.css';
+import assets from '../../assets/assets';
+import { useState } from 'react';
 
 const Profile_Update = () => {
+  const [image, setImage] = useState(false);
   return (
-    <div>
-      <h2>We are on the Profile page</h2>
+    <div className="profile">
+      <div className="profile-container">
+        <form>
+          <h2>Profile Details</h2>
+          <label htmlFor="avatar">
+            <input
+              onChange={e => setImage(e.target.files[0])}
+              type="file"
+              id="avatar"
+              accept=".jpg , .png, .jpeg"
+              hidden
+            />
+            <img
+              src={image ? URL.createObjectURL(image) : assets.avatar_icon}
+              alt=""
+            />
+            upload profile image
+          </label>
+          <input type="text" placeholder="Your Name" required />
+          <textarea placeholder="Write Profile Bio" required></textarea>
+          <button type="submit">Save</button>
+        </form>
+
+        <img
+          className="profile-pic"
+          src={image ? URL.createObjectURL(image) : assets.logo_icon}
+          alt=""
+        />
+      </div>
     </div>
   );
 };
